d82676385fd7c66059dc07be0c895fdf06a5d839
Ballistic445 <Ethan.kelly46@outlook.com>
2025-03-19 12:49:53 +0000

    created start of snake

--- code/main.py
+++ code/main.py
@@ -1,1 +1,23 @@
+import pygame
 
+pygame.init()
+
+true = True
+false = False
+
+WindowWidth = 720
+WindowHeight = 256
+screen = pygame.display.set_mode((WindowWidth, WindowHeight))
+clock = pygame.time.Clock()
+
+Running = true
+
+while Running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            Running = false
+
+    screen.fill("black")
+    pygame.display.flip()
+
+    clock.tick(60)
